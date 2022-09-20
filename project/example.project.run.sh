049d1d68c7b5e04b28c1200b2fc7f116e353b171
nullcap <anderton601@gmail.com>
2022-09-19 21:28:31 -0500

    comment and documentation editing

--- project/example.project.run.sh
+++ project/example.project.run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## Prep-work: Remove files, execute other scripts within the project directory structure, create result directories, etc. 
+## Prep-work: Remove files, execute other scripts within the project directory structure, create directories, etc. 
 echo "Begin Runscript"
 rm -rf results/$Project/old_results/
 
@@ -8,10 +8,10 @@ current_time = $(date +'%m_%d_%H')
 mkdir -p results/$Project/results/$current_time/
 
 ## Execute the Unsteady binary. Include the "time" command to receive a printout of the time it took to run start to finish.
-time RasUnsteady Project.c02 b08
+time RasUnsteady MyProject.c02 b08
 
 ## Once the run is complete, copy the needed files to the results directory to be offloaded from the container. 
-rsync -av Project.p08.tmp.hdf results/$PROJECT/results/$current_time/Project.p08.hdf
+rsync -av MyProject.p08.tmp.hdf results/$PROJECT/results/$current_time/Project.p08.hdf
 
 ## Not required, but helps to see the end of your script sometimes. 
 echo "Runscript Complete"
